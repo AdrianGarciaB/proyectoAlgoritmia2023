c569833ae8df893e74401c5b41ca6b5896fde209
houda.el.fezzak <houda.el.fezzak@estudiantat.upc.edu>
2023-09-30 23:03:07 +0200

    Fix

--- kdTree.cc
+++ kdTree.cc
@@ -34,7 +34,7 @@ BoundingBox kdTree::rootBoundingBox() {
 
 Node* kdTree::i_insert(Node*& root, const vector<double>& info, unsigned depth) {
     if (root == nullptr) {
-        BoundingBox rootBBox = rootBoundingBox(info);
+        BoundingBox rootBBox = rootBoundingBox();
         root = new Node(info, nullptr, nullptr, rootBBox);
         return root;
     }
@@ -78,10 +78,10 @@ Node* kdTree::i_insertWithBoundingBox(Node*& root, const vector<double>& info, u
 
         if (info[axis] < root->x[axis]) {
             nextBBox.maxPoint[axis] = root->x[axis];
-            root->left = i_insertWithBoundingBox(root->left, info, depth + 1, nextBBox);
+            root->left = i_insertWithBoundingBox(root->left, info, depth + 1);
         } else {
             nextBBox.minPoint[axis] = root->x[axis];
-            root->right = i_insertWithBoundingBox(root->right, info, depth + 1, nextBBox);
+            root->right = i_insertWithBoundingBox(root->right, info, depth + 1);
         }
 
         return root;
