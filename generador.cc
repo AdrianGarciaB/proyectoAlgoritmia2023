87ca7cb33396652f03f489bf3e49482a98d99f48
3LVcT50r <vhb575@gmail.com>
2023-10-01 19:19:08 +0200

    Los valores se guardan en vector

--- generador.cc
+++ generador.cc
@@ -1,26 +1,39 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 using namespace std;
+
+typedef vector<double> Coord;
 
 int main() {
     int T;      //num de arboles a generar
     int k;      //dimension de cada arbol
     int n;      //num de nodos de cada arbol
 
+    cout << "Numero de arboles:" << endl;
     cin >> T;
 
     srand(time(NULL));
 
     for(int i = 1; i <= T; ++i) {   
-        cin >> k >> n;
+
+        cout << "Dimension:" << endl;
+        cin >> k;
+        cout << "Numero de nodos:" << endl;
+        cin >> n;
+
+        vector<Coord> Coords(n, Coord(k));
+
         cout << "Arbol #" << i << ": " << k << " dimensiones y " << n << " nodos" << endl;
 
         for(int j = 0; j < n; ++j) {
             for(int l = 0; l < k; ++l) {
-                cout << double(rand()/double(RAND_MAX)) << " ";
+                double val = double(rand()/double(RAND_MAX));
+                Coords[j][l] = val;
+                cout << val << " ";
             }
             cout << endl;
         }
-        cout << endl << endl << "----------------------------------" << endl << endl; 
+        cout << endl << endl << "----------------------------------" << endl << endl;
     }
 }
