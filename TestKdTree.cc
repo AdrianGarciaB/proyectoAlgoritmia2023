e48cb62f57f9549ecc44f4480bcc17aa900cc243
AdrianGarciaB <adrian.garcia.belmonte@estudiantat.upc.edu>
2023-10-04 16:29:27 +0200

    Añadido pequeño test basico para una dimension (K1)

--- TestKdTree.cc
+++ TestKdTree.cc
@@ -22,6 +22,44 @@ EXPECT_TRUE(tree->empty());
 EXPECT_EQ(tree->valori(0), -1); // Asumiendo que un árbol vacío retorna -1 para cualquier coordenada.
 }
 
+TEST_F(KDTreeTest, TestBasicK1InsertAndInorder) {
+EXPECT_TRUE(tree);
+vector<double> point1 = {0.1};
+vector<double> point2 = {0.2};
+vector<double> point3= {0.3};
+vector<double> point4= {0.4};
+vector<double> point5 = {0.5};
+
+tree->insert(point3);
+tree->insert(point2);
+tree->insert(point4);
+tree->insert(point5);
+EXPECT_FALSE(tree->empty());
+EXPECT_EQ(tree->getRoot()->x[0], 0.3);
+EXPECT_EQ(tree->getRoot()->left->x[0], 0.2);
+EXPECT_EQ(tree->getRoot()->right->x[0], 0.4);
+EXPECT_EQ(tree->getRoot()->right->right->x[0], 0.5);
+}
+
+TEST_F(KDTreeTest, TestBasicK2InsertAndInorder) {
+EXPECT_TRUE(tree);
+vector<double> point1 = {0.1};
+vector<double> point2 = {0.2};
+vector<double> point3= {0.3};
+vector<double> point4= {0.4};
+vector<double> point5 = {0.5};
+
+tree->insert(point3);
+tree->insert(point2);
+tree->insert(point4);
+tree->insert(point5);
+EXPECT_FALSE(tree->empty());
+EXPECT_EQ(tree->getRoot()->x[0], 0.3);
+EXPECT_EQ(tree->getRoot()->left->x[0], 0.2);
+EXPECT_EQ(tree->getRoot()->right->x[0], 0.4);
+EXPECT_EQ(tree->getRoot()->right->right->x[0], 0.5);
+}
+
 TEST_F(KDTreeTest, TestInsertAndInorder) {
 EXPECT_TRUE(tree);
 vector<double> point1 = {1.0, 2.0};
